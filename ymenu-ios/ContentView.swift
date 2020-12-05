e3279aa3a483ceac096e492309f7d088e16020a9
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-12-05 14:10:56 +0100

    Commented the AR view

--- ymenu-ios/ContentView.swift
+++ ymenu-ios/ContentView.swift
@@ -10,28 +10,36 @@ import RealityKit
 
 struct ContentView : View {
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+//        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        
+        Home()
     }
 }
 
-struct ARViewContainer: UIViewRepresentable {
-    
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
-        return arView
-        
+//struct ARViewContainer: UIViewRepresentable {
+//
+//    func makeUIView(context: Context) -> ARView {
+//
+//        let arView = ARView(frame: .zero)
+//
+//        // Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! Experience.loadBox()
+//
+//        // Add the box anchor to the scene
+//        arView.scene.anchors.append(boxAnchor)
+//
+//        return arView
+//
+//    }
+//
+//    func updateUIView(_ uiView: ARView, context: Context) {}
+//
+//}
+
+struct Home: View {
+    var body: some View{
+        Text("Home")
     }
-    
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
 }
 
 #if DEBUG
