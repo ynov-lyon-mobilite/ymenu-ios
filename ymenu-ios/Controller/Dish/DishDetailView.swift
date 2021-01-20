917323df64ef06b44f4567577c3e35609cfa891f
Antoine Mousset <amousset@secuserve.com>
2021-01-20 15:34:42 +0100

    Working :) (manque plus que les photos)

--- ymenu-ios/Controller/Dish/DishDetailView.swift
+++ ymenu-ios/Controller/Dish/DishDetailView.swift
@@ -10,12 +10,28 @@ import SwiftUI
 struct DishDetailView: View {
 
     @State var dish: Dish
+    @ObservedObject var viewModel = MenuViewModel()
+   
 
     var body: some View {
         VStack(alignment: .leading) {
+           
             HStack {
-                Text(dish.name)
+                List {
+                    let ingredients = dish.ingredients
+                        Text(dish.name)
+                            .font(.system(size: 26, weight: .bold, design: .default))
+                            .foregroundColor(Color.red)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .multilineTextAlignment(.center)
+                    Text("Ingrédients:")
+                        .font(.system(size: 17, weight: .bold, design: .default))
+                    ForEach(ingredients!, id: \.self) { ingredient in
+                                   Text(ingredient)
+                               }
                     .padding(.trailing, 5)
+                }
+                .padding(.horizontal, -20)
             }
         }
         .padding()
