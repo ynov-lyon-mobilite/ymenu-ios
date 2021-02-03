a09a44fc16721cc6afff030efffa85ed4d21c478
Zakarya Tolba <zakarya.tolba@ynov.com>
2021-02-03 22:29:27 +0100

    Added image to DishDetailView

--- ymenu-ios/Controller/Dish/DishDetailView.swift
+++ ymenu-ios/Controller/Dish/DishDetailView.swift
@@ -6,24 +6,42 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct DishDetailView: View {
 
     @State var dish: Dish
     @ObservedObject var viewModel = MenuViewModel(restaurant: RestaurantDTO(_id: "", name: ""))
-   
 
     var body: some View {
+        let ingredients = dish.ingredients
+        
         VStack(alignment: .leading) {
            
             HStack {
                 List {
-                    let ingredients = dish.ingredients
-                        Text(dish.name)
-                            .font(.system(size: 26, weight: .bold, design: .default))
-                            .foregroundColor(Color.red)
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .multilineTextAlignment(.center)
+                    WebImage(url: URL(string: dish.url_logo))
+                      .onSuccess { image, data, cacheType in
+                      }
+                      .resizable()
+                      .placeholder {
+                          Rectangle().foregroundColor(.gray)
+                      }
+                      .indicator(.activity)
+                      .transition(.fade(duration: 0.5))
+                      .scaledToFit()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 400, height: 280, alignment: .center)
+                        .cornerRadius(30)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .shadow(radius: 6, x: 3, y: 3)
+                    Text(dish.name)
+                        .padding(.top, 10)
+                        .padding(.bottom, 10)
+                        .font(.system(size: 26, weight: .bold, design: .default))
+                        .foregroundColor(Color.red)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .multilineTextAlignment(.center)
                     Text("Ingrédients:")
                         .font(.system(size: 17, weight: .bold, design: .default))
                     ForEach(ingredients!, id: \.self) { ingredient in
