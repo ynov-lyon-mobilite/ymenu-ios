7f918672980b6eac3b979e7ae2e68f9350421b2e
Léo GIGANT <lg@bsoft.fr>
2021-02-04 13:40:49 +0100

    Added price to product detail & added AR button

--- ymenu-ios/Controller/Dish/DishDetailView.swift
+++ ymenu-ios/Controller/Dish/DishDetailView.swift
@@ -10,6 +10,7 @@ import SDWebImageSwiftUI
 
 struct DishDetailView: View {
     var dish: Dish
+    @State var isPresented = false
     
     init(dish: Dish) {
         UINavigationBar.appearance().largeTitleTextAttributes = [
@@ -23,39 +24,78 @@ struct DishDetailView: View {
         let ingredients = dish.ingredients
         
         VStack(alignment: .leading) {
-           
-            HStack {
-                List {
-                    WebImage(url: URL(string: dish.url_logo))
-                      .onSuccess { image, data, cacheType in
-                      }
-                      .resizable()
-                      .placeholder {
-                          Rectangle().foregroundColor(.gray)
-                      }
-                      .indicator(.activity)
-                      .transition(.fade(duration: 0.5))
-                      .scaledToFit()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 400, height: 280, alignment: .center)
-                        .cornerRadius(30)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .shadow(radius: 6, x: 3, y: 3)
-                    Text(dish.name)
-                        .padding(.top, 10)
-                        .padding(.bottom, 10)
-                        .font(.custom("SF Pro Text Bold", fixedSize: 22))
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .multilineTextAlignment(.center)
-                    Text("Ingrédients :")
-                        .font(.custom("SF Pro Text Bold", fixedSize: 17))
-                    ForEach(ingredients!, id: \.self) { ingredient in
-                        Text(ingredient)
-                            .font(.custom("SF Pro Text Regular", fixedSize: 17))
-                    }
-                    .padding(.trailing, 5)
+            ZStack {
+                HStack {
+                    List {
+                        WebImage(url: URL(string: dish.url_logo))
+                          .onSuccess { image, data, cacheType in
+                          }
+                          .resizable()
+                          .placeholder {
+                              Rectangle().foregroundColor(.gray)
+                          }
+                          .indicator(.activity)
+                          .transition(.fade(duration: 0.5))
+                          .scaledToFit()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 400, height: 280, alignment: .center)
+                            .cornerRadius(30)
+                            .shadow(radius: 6, x: 3, y: 3)
+                        HStack {
+                            Text(dish.name)
+                            .padding(.top, 10)
+                            .padding(.bottom, 10)
+                            .font(.system(size: 26, weight: .bold, design: .default))
+                            .foregroundColor(Color.red)
+                            Spacer()
+                            Text("\(String(describing: dish.price))0 €")
+                            .padding(.top, 10)
+                            .padding(.bottom, 10)
+                            .font(.system(size: 26, weight: .bold, design: .default))
+                        }
+                        Text("Ingrédients:")
+                            .font(.system(size: 17, weight: .bold, design: .default))
+                        ForEach(ingredients!, id: \.self) { ingredient in
+                                       Text(ingredient)
+                        }
+                        .padding(.trailing, 5)
+                   }.padding(.horizontal, -20)
                 }
-                .padding(.horizontal, -20)
+                Button(action: {
+                    self.isPresented = true
+                }) {
+                HStack {
+                    Image(systemName: "arkit")
+                    Text("Voir en RA")
+                        .bold()
+                }}
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.orange)
+                .cornerRadius(.greatestFiniteMagnitude)
+                .sheet(isPresented: $isPresented) {
+                    ZStack (alignment: .topLeading){
+                        ARQuickLookView(fileName: "minuteasia_bobun", title: "Bobun", isPresented: self.$isPresented).edgesIgnoringSafeArea(.bottom)
+                        HStack (alignment: .top){
+                            VStack (alignment: .leading) {
+                                Button(action: {
+                                 self.isPresented = false
+                                }, label: {
+                                Image(systemName: "xmark")
+                                    .font(.system(size: 25, weight: .light, design: .rounded))
+                                    .padding(.horizontal, 19)
+                                    .padding(.vertical, 12)
+                                    .background(
+                                        Blur(
+                                             style: .systemThinMaterial)
+                                        )
+                                }).buttonStyle(PlainButtonStyle()).cornerRadius(14).padding(14).padding(.top, 11)
+                             }
+                         }
+                    }
+                }.shadow(radius: 5, x: 2, y: 2
+                .frame(maxHeight: .infinity, alignment: .bottom)
+                Spacer()
             }
         }
         .padding()
