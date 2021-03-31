566b4ea54e1c7d6f20749dc71f00b4ca8a5fec21
enzo sborea <enzo.sborea@outlook.com>
2021-03-31 15:14:38 +0200

    Profil / WIP
    Navigation ok, couleur retour(navigation) ok, manque design bouton

--- ymenu-ios/Controller/Profil/InfoProfilView.swift
+++ ymenu-ios/Controller/Profil/InfoProfilView.swift
@@ -1,17 +1,14 @@
 //
-//  DishDetailView.swift
+//  InfoProfilView.swift
 //  ymenu-ios
 //
-//  Created by Léo GIGT on 20/01/2021.
+//  Created by Enzo Sborea on 31/03/2021.
 //
 
 import SwiftUI
 
-struct ProfilView: View {
-    @State var isPresented = false
-
+struct InfoProfilView: View {
     var body: some View {
-        NavigationView{
         ZStack{
             Color(UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 0.1))
                     .edgesIgnoringSafeArea(.all)
@@ -35,7 +32,7 @@ struct ProfilView: View {
                 .offset(x: 160)
             }
             VStack{
-            Text("Profil")
+            Text("Mes informations")
                 .font(.custom("SF Pro Text Regular", size: 30))
                 .multilineTextAlignment(.center)
                 .lineSpacing(5)
@@ -44,47 +41,51 @@ struct ProfilView: View {
             VStack{
                 RoundedRectangle(cornerRadius: 100)
                     .frame(width: 300, height: 55)
+    //                    .foregroundColor(Color(red: 235/255, green: 165/255, blue: 60/255))
                     .foregroundColor(Color.orange)
                     .padding()
                         .shadow(radius: 5, x: 2, y: 2)
                         .border(Color.black, width: 0)
                     .overlay(
-                        NavigationLink(
-                            destination: InfoProfilView(), // <3>
-                                label: {
-                                    Label("Mes informations", systemImage: "") // <7>
-                                    })
+                        Text("Nom")
                         .foregroundColor(.white)
                             .font(.custom("SF Pro Text Bold", size: 24))
                     ).offset(y:20)
-                }
-            
+            }.padding()
             VStack{
                 RoundedRectangle(cornerRadius: 100)
                     .frame(width: 300, height: 55)
+    //                    .foregroundColor(Color(red: 235/255, green: 165/255, blue: 60/255))
                     .foregroundColor(Color.orange)
                     .padding()
                         .shadow(radius: 5, x: 2, y: 2)
                         .border(Color.black, width: 0)
                     .overlay(
-                        NavigationLink(
-                            // destination: mettre view historique
-                            destination: InfoProfilView(), // <3>
-                                label: {
-                                    Label("Historique de visite", systemImage: "") // <7>
-                                    })
+                        Text("Prénom")
                         .foregroundColor(.white)
                             .font(.custom("SF Pro Text Bold", size: 24))
-                    ).offset(y: 120)
-                }
-            .navigationBarTitle("Profil")
-            .navigationBarHidden(true)
+                    ).offset(y:110)
+            }.padding()
+            VStack{
+                RoundedRectangle(cornerRadius: 100)
+                    .frame(width: 300, height: 55)
+    //                    .foregroundColor(Color(red: 235/255, green: 165/255, blue: 60/255))
+                    .foregroundColor(Color.orange)
+                    .padding()
+                        .shadow(radius: 5, x: 2, y: 2)
+                        .border(Color.black, width: 0)
+                    .overlay(
+                        Text("Email")
+                        .foregroundColor(.white)
+                            .font(.custom("SF Pro Text Bold", size: 24))
+                    ).offset(y:200)
+            }.padding()
         }.edgesIgnoringSafeArea(.all)
-        }.accentColor(.orange)
     }
-}
-struct ProfilView_Previews: PreviewProvider {
+    }
+
+struct InfoProfilView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfilView()
+        InfoProfilView()
     }
 }
