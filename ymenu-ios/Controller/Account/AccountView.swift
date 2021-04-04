8505f921a6dba4b527cdfec40a57bff2cb33d007
Zakarya Tolba <zakarya.tolba@ynov.com>
2021-04-04 15:03:14 +0200

    Created AccountView

---
+++ ymenu-ios/Controller/Account/AccountView.swift
@@ -0,0 +1,44 @@
+//
+//  AccountView.swift
+//  ymenu-ios
+//
+//  Created by Zakarya TOLBA on 31/03/2021.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    
+    @ObservedObject var applicationState: ApplicationState = ApplicationState.shared
+    
+    var body: some View {
+        ZStack {
+            Group {
+                switch applicationState.state {
+                case .authenticated:
+                    Button(action: {
+                        applicationState.disconnect()
+                        print(applicationState.state)
+                    }) {
+                        HStack {
+                            Text("Déconnexion")
+                                .bold()
+                        }}.padding()
+                    .foregroundColor(.white)
+                    .padding([.trailing, .leading], 50)
+                    .background(Color.themeTextField)
+                    .cornerRadius(.greatestFiniteMagnitude)
+                    .shadow(radius: 6, x: 3, y: 3)
+                    .padding(.top, 40)
+                case .loading:
+                    Text("Loading ...")
+                case .requireLogin:
+                    LoginView(viewModel: LoginViewModel())
+                }
+            }
+        }.onAppear(perform: {
+            applicationState.startApplication()
+        })
+    }
+}
+
