a28f77a96a1b3128c4a2d72b2d71984a6da590e4
Zakarya Tolba <zakarya.tolba@ynov.com>
2021-04-10 00:17:34 +0200

    Temporary set HistoryView as authenticated view state

--- ymenu-ios/Controller/Account/AccountView.swift
+++ ymenu-ios/Controller/Account/AccountView.swift
@@ -17,20 +17,7 @@ struct AccountView: View {
             Group {
                 switch applicationState.state {
                 case .authenticated:
-                    Button(action: {
-                        applicationState.disconnect()
-                        print(applicationState.state)
-                    }) {
-                        HStack {
-                            Text("Déconnexion")
-                                .bold()
-                        }}.padding()
-                    .foregroundColor(.white)
-                    .padding([.trailing, .leading], 50)
-                    .background(Color.themeTextField)
-                    .cornerRadius(.greatestFiniteMagnitude)
-                    .shadow(radius: 6, x: 3, y: 3)
-                    .padding(.top, 40)
+                    HistoryView(viewModel: HistoryViewModel())
                 case .loading:
                     Text("Loading ...")
                 case .requireLogin:
