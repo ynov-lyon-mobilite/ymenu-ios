79317981bda83dc52d51f6fecca94592e881c50f
Zakarya Tolba <zakarya.tolba@ynov.com>
2021-04-10 19:22:42 +0200

    Removed temporary disconnect button from history view & changed title to automatic

--- ymenu-ios/Controller/History/HistoryView.swift
+++ ymenu-ios/Controller/History/HistoryView.swift
@@ -26,88 +26,63 @@ struct HistoryView: View {
             ProgressView("Chargement de l'historique")
                 .zIndex(1)
         }
-        NavigationView {
-            ScrollViewReader { proxy in
-                VStack{
-                    List {
-                        ForEach(viewModel.restaurants, id: \.self) { restaurant in
-                            HStack {
-                                Button(action: {
-                                    DispatchQueue.main.async {
-                                        self.viewRouter.restaurant = RestaurantDTO(_id: restaurant._id, name: restaurant.name)
-                                        withAnimation {
-                                            self.viewRouter.currentPage = "greetingcard.fill"
-                                        }
+        ZStack {
+            VStack {
+                List {
+                    ForEach(viewModel.restaurants, id: \.self) { restaurant in
+                        HStack {
+                            Button(action: {
+                                DispatchQueue.main.async {
+                                    self.viewRouter.restaurant = RestaurantDTO(_id: restaurant._id, name: restaurant.name)
+                                    withAnimation {
+                                        self.viewRouter.currentPage = "greetingcard.fill"
                                     }
-                                }){
-                                    WebImage(url: URL(string: restaurant.url_logo))
-                                        .onSuccess { image, data, cacheType in
-                                        }
-                                        .resizable()
-                                        .placeholder {
-                                            Rectangle().foregroundColor(.gray)
-                                        }
-                                        .indicator(.activity)
-                                        .transition(.fade(duration: 0.5))
-                                        .scaledToFit()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: 150, height: 95, alignment: .center)
-                                        .cornerRadius(13)
-                                        .frame(maxWidth: .infinity, alignment: .center)
-                                        .shadow(radius: 6, x: 3, y: 3)
-                                        .padding(.leading, -30).padding(.trailing, 10)
-                                    VStack(alignment: .center){
-                                        Text(restaurant.name).bold()
-
-                                        Text(restaurant.address).font(.system(size: 16, weight: .light, design: .rounded)).multilineTextAlignment(.center)
-                            
-                                    }.padding(.leading, 5).padding(.trailing, 5).frame(maxWidth: .infinity, maxHeight: .infinity)
-                                    .contentShape(Rectangle())
-                     
                                 }
-                            }.padding()
-                        }.onAppear{
-                            self.historyLoaded = true
-                        }
+                            }){
+                                WebImage(url: URL(string: restaurant.url_logo))
+                                    .onSuccess { image, data, cacheType in
+                                    }
+                                    .resizable()
+                                    .placeholder {
+                                        Rectangle().foregroundColor(.gray)
+                                    }
+                                    .indicator(.activity)
+                                    .transition(.fade(duration: 0.5))
+                                    .scaledToFit()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 150, height: 95, alignment: .center)
+                                    .cornerRadius(13)
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                                    .shadow(radius: 6, x: 3, y: 3)
+                                    .padding(.leading, -30).padding(.trailing, 10)
+                                VStack(alignment: .center){
+                                    Text(restaurant.name).bold()
+
+                                    Text(restaurant.address).font(.system(size: 16, weight: .light, design: .rounded)).multilineTextAlignment(.center)
+                        
+                                }.padding(.leading, 5).padding(.trailing, 5).frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .contentShape(Rectangle())
+                 
+                            }
+                        }.padding()
+                    }.onAppear{
+                        self.historyLoaded = true
                     }
-                    .navigationBarTitle("Historique", displayMode: .large)
+                }
+                .navigationBarTitle(Text("Historique"), displayMode: .automatic)
 //                    .navigationBarItems(
 //                        leading:
 //                            Text("adresse mail".uppercased()).bold()
 //                            .padding(.top, 23)
 //                            .font(.footnote)
 //                            .foregroundColor(Color(.systemGray)))
-                }
-                .listStyle(InsetGroupedListStyle())
-                .progressViewStyle(CircularProgressViewStyle())
-                Button(action: {
-                    self.showAlert = true
-                }) {
-                    HStack {
-                        Text("Déconnexion")
-                            .bold()
-                    }}.padding()
-                .foregroundColor(.white)
-                .padding([.trailing, .leading], 50)
-                .background(Color.themeTextField)
-                .cornerRadius(.greatestFiniteMagnitude)
-                .shadow(radius: 6, x: 3, y: 3)
-                .padding(.top, 40)
             }
+            .listStyle(InsetGroupedListStyle())
+            .progressViewStyle(CircularProgressViewStyle())
         }.buttonStyle(PlainButtonStyle()).onAppear {
             UINavigationBar.appearance().largeTitleTextAttributes = [
                 .font : UIFont(name:"SF Pro Rounded Bold", size: 40)!
             ]
-        }
-        .alert(isPresented: $showAlert) {
-            Alert(title: Text("Déconnexion"),
-             message: Text("Êtes-vous sûr(e) de vouloir vous déconnecter?"),
-             primaryButton: .cancel(Text("Annuler")),
-             secondaryButton: .destructive(Text("Se déconnecter"), action: {
-                withAnimation {
-                    applicationState.disconnect()
-                }
-             }))
         }
     }
 }
